23ae77d0912f6f2833ca4551f887e6831d8fc5b7
Ginele Tanis <gt1113@nyu.edu>
2019-10-31 11:15:26 -0400

    List of Guns for the first 2 missions

---
+++ Assets/ScriptableObjects/Guns.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Guns : ScriptableObject
+{
+    public GameObject GunFire; 
+    public Sprite SelectedWeapon;
+    public AudioSource AS;
+    public AudioClip Shot; 
+    public float FireSpeed;
+    public bool Automatic;
+    public float Spread;
+    public int bulletNumber;
+    public float bulletVel;
+    public int Damage; 
+
+}
