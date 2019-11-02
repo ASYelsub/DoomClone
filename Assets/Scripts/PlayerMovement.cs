4fe319c71811be54f459795553cdbde9c169e0f3
Kirijo <asyelsub@gmail.com>
2019-11-02 13:36:13 -0400

    Some push?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,29 +7,42 @@ public class PlayerMovement : MonoBehaviour
     //public Transform playerTransform;
     Transform cameraTransform;
     float timerW,timerA,timerS,timerD = 40f;
+    float defaultHeight;
+    float walkTime;
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = GetComponent<Transform>();
+        defaultHeight = cameraTransform.parent.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
 
         Vector3 mouseRotate = new Vector3(0f,mouseX*10f,0f);
         Vector3 dRotate = new Vector3(0f,10f,0f);
         Vector3 aRotate = new Vector3(0f,-10f,0f);
+        
         cameraTransform.parent.Rotate(mouseRotate);
 
         if(mouseY > 0){
-            cameraTransform.parent.position = cameraTransform.parent.position + cameraTransform.parent.forward*0.5f;
+            cameraTransform.parent.position = cameraTransform.parent.position + cameraTransform.parent.forward*0.5f;            
         }
-        if(mouseY <0){
+        if(mouseY < 0){
             cameraTransform.parent.position = cameraTransform.parent.position - cameraTransform.parent.forward*0.5f;
         }
+        if(Mathf.Abs(mouseY) > 0){
+            walkTime += Time.deltaTime;
+        }
+        cameraTransform.parent.position = new Vector3(cameraTransform.parent.position.x, defaultHeight + Mathf.Cos(walkTime*5f)*0.8f, cameraTransform.parent.position.z);
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            Debug.Log(mouseY);
+        }
+        
 
         if(Input.GetKey(KeyCode.W)){
             //Vector3 keyForward = new Vector3(forward);
