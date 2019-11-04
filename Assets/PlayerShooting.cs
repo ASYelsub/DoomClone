aa0c830f1957c25b66c3e6432c34ae86a7cdbd41
Cooper Yang - Mac <>
2019-11-04 15:19:14 -0500

    Fuck Yeah!

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerShooting : MonoBehaviour
 {
+    public List<GameObject> enemyList;
+    public LayerMask obstacleLayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,28 @@ public class PlayerShooting : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        DetectNShoot();
+    }
+
+    void DetectNShoot(){
         
+        foreach(GameObject enemy in enemyList){
+            Vector3 enemyPos = enemy.transform.position;
+            Vector3 myPos = transform.position;
+
+            enemyPos.y = 0;
+            myPos.y = 0;
+
+            Vector3 targetDir = enemyPos-myPos;
+
+            if(Vector3.Angle(targetDir,transform.forward)<3f){
+                //Debug.Log("Detect!");
+                if(!Physics.Linecast(transform.position,enemy.transform.position,obstacleLayer)){
+                    Debug.Log("Hit");
+                }
+            }
+        }
     }
 }
