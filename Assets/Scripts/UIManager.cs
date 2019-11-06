f560a56bcf5623a2a705b3678c445e6d011d3897
Kirijo <asyelsub@gmail.com>
2019-11-06 18:41:59 -0500

    Continuation of UI

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text ammoText;
+    public Text healthText;
+    public Text armorText;
+    public Text bullText;
+    public Text shellText;
 
-    // Update is called once per frame
-    void Update()
+    public void ArmorEdit(int armor)
     {
-        
+        armorText.text = armor.ToString();
+    }
+    public void HealthEdit(int health)
+    {
+        healthText.text = health.ToString();
     }
 }
