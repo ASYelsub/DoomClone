6ac2e8e76bdd342ab22905bc3dad39ebece81a37
Cooper Yang - Mac <>
2019-11-13 18:08:02 -0500

    Movement and resonance

--- Assets/Scripts/DemoPlayerController.cs
+++ Assets/Scripts/DemoPlayerController.cs
@@ -4,7 +4,9 @@
 public class DemoPlayerController : MonoBehaviour {
   public Camera mainCamera;
   private CharacterController characterController = null;
-  private float movementSpeed = 5.0f;
+  public float movementSpeed = 1.0f;
+  public float rotationSensitivity = 1.0f;
+  public float gravity = 20f;
 
   // // Target camera rotation in degrees.
   // private float rotationX = 0.0f;
@@ -49,12 +51,20 @@ public class DemoPlayerController : MonoBehaviour {
     
     
     // Update the position.
-    //float movementX = Input.GetAxis("Horizontal");
+    float movementX = Input.GetAxis("Horizontal");
     float movementY = Input.GetAxis("Vertical");
     Vector3 movementDirection = new Vector3(0.0f, 0.0f, movementY);
-    movementDirection = mainCamera.transform.localRotation * movementDirection;
+    
+    
+    transform.Rotate(new Vector3(0.0f,movementX*rotationSensitivity, 0.0f));
+
+    movementDirection = transform.localRotation * movementDirection;
     movementDirection.y = 0.0f;
-    characterController.SimpleMove(movementSpeed * movementDirection);
+    movementDirection *= movementSpeed;
+    
+    movementDirection.y -= gravity * Time.deltaTime;
+    characterController.Move(movementDirection * Time.deltaTime);
+    
   }
 
   // Sets the cursor lock for first-person control.
