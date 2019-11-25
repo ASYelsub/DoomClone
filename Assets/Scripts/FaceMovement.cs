9fbf74504e111c99fc38548027f8b172ae340ec0
Kirijo <asyelsub@gmail.com>
2019-11-25 16:39:53 -0500

    Implement ginele's map and attempt to get man looking back and forth

---
+++ Assets/Scripts/FaceMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FaceMovement : MonoBehaviour
+{
+    public Sprite lookLeft;
+    public Sprite lookRight;
+    public Image image;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Image image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void changeRight()
+    {
+
+    }
+    void changeLeft()
+    {
+
+    }
+    IEnumerator BackAndForth()
+    {
+       
+        yield return new WaitForSeconds(10f);
+       
+    }
+}
