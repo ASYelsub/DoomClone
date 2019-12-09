fd4173235141d1c8c6eb7861539ac485cc37d2f4
Cooper Yang <yangcooper@me.com>
2019-12-09 17:24:03 -0500

    Some tweaking on player shooting and sound

---
+++ Assets/Scripts/BarrelScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelScript : MonoBehaviour
+{
+    public int health;
+
+
+    private void Start()
+    {
+        health = 3;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0)
+        {
+            SoundMan.me.BarrelExplode(transform.position);
+        }
+    }
+}
